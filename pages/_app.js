76c4fb025871a14cfce44ecb3bc894fc82e03da7
growwithgm <punjabi9001226@gmail.com>
2025-08-17 16:40:06 +0200

    Delete pages directory

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '@/styles/globals.css';
-
-// The custom App component enables us to import global CSS files.
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
